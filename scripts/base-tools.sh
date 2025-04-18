55d768be1752e5fb148b9722ae176c86f31fdb77
TomSec <43483906+tomsec8@users.noreply.github.com>
2025-04-18 18:14:32 +0300

    Update base-tools.sh

--- scripts/base-tools.sh
+++ scripts/base-tools.sh
@@ -1,1 +1,6 @@
+#!/data/data/com.termux/files/usr/bin/bash
+# Installs base Termux tools
+
+echo "[*] Installing base tools..."
+pkg install -y root-repo x11-repo curl wget nano zip unzip python net-tools python-pip unstable-repo grep httping  && apt autoremove -y
 
